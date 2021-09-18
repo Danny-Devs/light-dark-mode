835d92c007aa388fa7c538b60523795729f5a200
Daniel Ahn <et3rnal.d@gmail.com>
2021-09-17 20:52:12 -0400

    refactor js

--- script.js
+++ script.js
@@ -5,34 +5,26 @@ const image3 = document.getElementById('image3');
 const toggleText = document.getElementById('toggle-text');
 const lightIcon = document.getElementById('light-icon');
 
-
-
-function activateLightMode() {
-  image1.src = "img/undraw_proud_coder_light.svg";
-  image2.src = "img/undraw_feeling_proud_light.svg";
-  image3.src = "img/undraw_conceptual_idea_light.svg";
-  toggleText.textContent = 'Light Mode';
-  lightIcon.classList.add('fa-sun');
-  lightIcon.classList.remove('fa-moon');
+function toggleLightMode(value) {
+  image1.src = `img/undraw_proud_coder_${value}.svg`;
+  image2.src = `img/undraw_feeling_proud_${value}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${value}.svg`;
+  toggleText.textContent = `${value.charAt(0).toUpperCase() + value.slice(1)} Mode`;
+  if ( value === 'light') {
+    lightIcon.classList.replace('fa-moon', 'fa-sun');
+  } else if ( value === 'dark') {
+    lightIcon.classList.replace('fa-sun', 'fa-moon');
+  }
 }
 
-function activateDarkMode() {
-  image1.src = "img/undraw_proud_coder_dark.svg";
-  image2.src = "img/undraw_feeling_proud_dark.svg";
-  image3.src = "img/undraw_conceptual_idea_dark.svg";
-  toggleText.textContent = 'Dark Mode';
-  lightIcon.classList.add('fa-moon');
-  lightIcon.classList.remove('fa-sun');
-}
-
-//  switch theme dynamically
+// switch theme dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    activateDarkMode();
+    toggleLightMode('dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    activateLightMode();
+    toggleLightMode('light');
   }
 }
 
